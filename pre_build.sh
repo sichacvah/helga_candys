4d69d3be286948d90395297848b256668dc38970
sichacvah <sichirc@gmail.com>
2016-02-10 19:47:33 +0400

    fix

--- pre_build.sh
+++ pre_build.sh
@@ -1,3 +1,4 @@
 mkdir ~/.aws
 touch ~/.aws/helga_candys.yml
-echo "access_key_id: $AWS_ACCESS_KEY \naccess_key_secret: $AWS_SECRET" >> ~/.aws/helga_candys.yml
+echo access_key_id: $AWS_ACCESS_KEY >> ~/.aws/helga_candys.yml
+echo access_key_secret: $AWS_SECRET >> ~/.aws/helga_candys.yml
