2a87262b6e9a6718935b6f9ae45fbee02ef355cf
sichacvah <sichirc@gmail.com>
2016-02-10 20:02:57 +0400

    fix

--- pre_build.sh
+++ pre_build.sh
@@ -1,3 +1,5 @@
+git config --global user.name "Sichacvah"
+git config --global user.email "sichirc@gmail.com"
 mkdir ~/.aws
 touch ~/.aws/helga_candys.yml
 echo access_key_id: $AWS_ACCESS_KEY >> ~/.aws/helga_candys.yml
