3394a465f9cbe782b5dcc6921bc55ac5bd5e2bda
Sichacvah <sichirc@gmail.com>
2016-02-13 12:47:35 +0400

    fix

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ activate :i18n, :mount_at_root => :ru, :no_fallbacks => true
 
 
 AWS_BUCKET = 'helgabakes.com'
-AWS_CLOUDFRONT_DISTRIBUTION_ID = '  E36WX0OE0KOWHO'
+AWS_CLOUDFRONT_DISTRIBUTION_ID = 'E36WX0OE0KOWHO'
 
 AWS_ACCESS_KEY = ENV['AWS_ACCESS_KEY']
 AWS_SECRET = ENV['AWS_SECRET']
@@ -28,7 +28,7 @@ activate :s3_sync do |s3_sync|
 	s3_sync.bucket = AWS_BUCKET
 	s3_sync.aws_access_key_id =  AWS_ACCESS_KEY
 	s3_sync.aws_secret_access_key = AWS_SECRET
-	s3_sync.region = 'us-west-2'
+	s3_sync.region = 'eu-central-1'
 	s3_sync.delete = true
 end
 
@@ -38,10 +38,6 @@ activate :cloudfront do |cf|
 	cf.distribution_id                  = AWS_CLOUDFRONT_DISTRIBUTION_ID
 end
 
-activate :deploy do |deploy|
-	deploy.method = :git
-	deploy.branch = "master"
-end
 
 activate :blog do |blog|
   blog.tag_template = "tag.html"
